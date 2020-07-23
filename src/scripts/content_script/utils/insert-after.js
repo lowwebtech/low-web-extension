5669702b8328c0464a511426f0bde80508193d56
vico - low web <lowwebtech@gmail.com>
2020-07-23 02:00:42 +0200

    comment unused code

--- src/scripts/content_script/utils/insert-after.js
+++
@@ -1,7 +0,0 @@
-export function insertAfter(newNode, currentNode) {
-  if (currentNode.nextSibling) {
-    currentNode.parentNode.insertBefore(newNode, currentNode.nextSibling);
-  } else {
-    currentNode.parentNode.appendChild(newNode);
-  }
-}
--- src/scripts/content_script/video/players/__DailymotionPlayer.js
+++ src/scripts/content_script/video/players/__DailymotionPlayer.js
@@ -1,50 +1,50 @@
-import { getDailymotionId } from '../../../utils/get-video-id';
+// import { getDailymotionId } from '../../../utils/get-video-id';
 
-export default class DailymotionPlayer {
-  constructor(iframe) {
-    this.originalIframe = iframe;
-    let params = {
-      video: getDailymotionId(this.originalIframe.dataset.src),
-      params: {
-        autoplay: true,
-        mute: true,
-      },
-    };
-    let qualityFirst = true;
+// export default class DailymotionPlayer {
+//   constructor(iframe) {
+//     this.originalIframe = iframe;
+//     let params = {
+//       video: getDailymotionId(this.originalIframe.dataset.src),
+//       params: {
+//         autoplay: true,
+//         mute: true,
+//       },
+//     };
+//     let qualityFirst = true;
 
-    if (this.originalIframe.width) {
-      params.width = this.originalIframe.width;
-    }
-    if (this.originalIframe.height) {
-      params.height = this.originalIframe.height;
-    }
+//     if (this.originalIframe.width) {
+//       params.width = this.originalIframe.width;
+//     }
+//     if (this.originalIframe.height) {
+//       params.height = this.originalIframe.height;
+//     }
 
-    this.newEl = document.createElement('div');
-    this.originalIframe.parentNode.replaceChild(this.newEl, this.originalIframe);
+//     this.newEl = document.createElement('div');
+//     this.originalIframe.parentNode.replaceChild(this.newEl, this.originalIframe);
 
-    this.player = window.DM.player(this.newEl, params);
-    this.player.addEventListener('apiready', () => {
-      // var quality = true
-      this.player.addEventListener('qualitiesavailable', () => {
-        // console.log('-- DAILYMOTION -- qualitiesavailable')
-        // console.log(this.player.quality)
-      });
-    });
-    this.player.addEventListener('playing', () => {
-      // console.log('-- DAILYMOTION -- playing', this.player.quality)
-      if (qualityFirst) {
-        // quality change seems to work after playing
-        qualityFirst = false;
-        this.player.pause();
-        this.player.setQuality('240');
-        this.player.play();
-      }
-    });
-    this.player.addEventListener('qualitychange', () => {
-      // console.log('-- DAILYMOTION -- qualitychange', this.player.quality)
-    });
-    this.player.addEventListener('rebuffer', () => {
-      // console.log('-- DAILYMOTION -- rebuffer')
-    });
-  }
-}
+//     this.player = window.DM.player(this.newEl, params);
+//     this.player.addEventListener('apiready', () => {
+//       // var quality = true
+//       this.player.addEventListener('qualitiesavailable', () => {
+//         // console.log('-- DAILYMOTION -- qualitiesavailable')
+//         // console.log(this.player.quality)
+//       });
+//     });
+//     this.player.addEventListener('playing', () => {
+//       // console.log('-- DAILYMOTION -- playing', this.player.quality)
+//       if (qualityFirst) {
+//         // quality change seems to work after playing
+//         qualityFirst = false;
+//         this.player.pause();
+//         this.player.setQuality('240');
+//         this.player.play();
+//       }
+//     });
+//     this.player.addEventListener('qualitychange', () => {
+//       // console.log('-- DAILYMOTION -- qualitychange', this.player.quality)
+//     });
+//     this.player.addEventListener('rebuffer', () => {
+//       // console.log('-- DAILYMOTION -- rebuffer')
+//     });
+//   }
+// }
