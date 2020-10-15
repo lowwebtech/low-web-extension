be81092d797ebbaada1bd2da0693c64c2e72321d
vico - low web <lowwebtech@gmail.com>
2020-10-16 01:32:59 +0200

    jest - unit test

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,16 +13,14 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
+    'plugin:vue/base',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard',
     // https://prettier.io/docs/en/index.html
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue', 'jest'],
   // add your custom rules here
   rules: {
     // allow async-await
