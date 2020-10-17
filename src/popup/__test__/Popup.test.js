de21c50e0d60e838c89d7f056796794d112f6b18
vico - low web <lowwebtech@gmail.com>
2020-10-17 19:59:34 +0200

    dotenv EXTENSION_INSTALLED (used for testing)

--- src/popup/__test__/Popup.test.js
+++ src/popup/__test__/Popup.test.js
@@ -5,52 +5,47 @@ const fs = require('fs');
 const puppeteer = require('puppeteer');
 const path = require('path');
 
-const withExtension = true;
 const pathToExtension = require('path').join(path.join(__dirname, '..', '..', '..', 'dist'));
 const puppeteerArgs = [];
-if ( withExtension ){
+
+if (process.env.EXTENSION_INSTALLED === 'true') {
   puppeteerArgs.push(`--disable-extensions-except=${pathToExtension}`, `--load-extension=${pathToExtension}`, '--show-component-extension-options');
 }
 
 let page, browser;
 describe('--------- Popup.html', () => {
-  beforeAll(async () => {
-    browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 250,
-      devtools: true,
-      args: puppeteerArgs,
+  if (process.env.EXTENSION_INSTALLED !== 'true') {
+    it('empty test', async () => {
+      expect('').toEqual('');
+    });
+  }else{
+    beforeAll(async () => {
+      browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 250,
+        devtools: true,
+        args: puppeteerArgs,
+      });
+
+      page = await browser.newPage();
+      screenShoter.setPage(page);
+
+      await goToPopupPage();
+      // await goToWeb('https://kuroneko.io/en/');
+    });
+    
+    afterAll(async () => {
+      // await polyserve.close();
+      await browser.close();
     });
 
-    page = await browser.newPage();
-    screenShoter.setPage(page);
-
-    await goToPopupPage();
-    // await goToWeb('https://kuroneko.io/en/');
-  });
-
-  afterAll(async () => {
-    // await polyserve.close();
-    await browser.close();
-  });
-
-  describe('------ INTEGRATION', () => {
-
-    describe('--- screenshots', () => {
-      if (withExtension) {
+    describe('------ INTEGRATION', () => {
+      describe('--- screenshots', () => {
         it('popup screenshot is equal to original', async () => {
           return screenShoter.takeAndCompare(page, 'popup.test');
         });
-      }
+      });
 
-      // it('kuroneko', async () => {
-      //   await goToWeb('https://kuroneko.io/en/');
-      //   await page.reload();
-      //   return screenShoter.takeAndCompare(page, 'kuroneko');
-      // });
-    });
-
-    if (withExtension) {
       describe('--- functional', () => {
         it('has white background color by default', async () => {
           const bodyColor = await page.$eval('body', (body) => body.style.backgroundColor);
@@ -74,8 +69,8 @@ describe('--------- Popup.html', () => {
           expect(bgColors).toEqual(['rgb(128, 128, 128)', 'rgb(97, 211, 22)']);
         });
       });
-    }
-  });
+    });
+  }
 });
 
 async function goToPopupPage() {
